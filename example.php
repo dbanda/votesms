ea2bc226bd6dbc17828e4b11ca99ed87e0688896
dalitso banda <dalitsohb@gmail.com>
2014-07-29 22:24:25 +0200

    debugging loop

--- example.php
+++ example.php
@@ -1,95 +1,6 @@
-<?php
-	error_reporting(E_ALL);
-	ini_set('display_errors',1);
-	// Create connection
-	//echo "trying to connect";
-	$con=mysqli_connect("127.0.0.1","root","root","votes");
-
-	// Check connection
-	if (mysqli_connect_errno()) {
-	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	}
-	else{
-		//echo "connected to mysql";
-	}
-	
-	include ( "src/NexmoMessage.php" );
-
-
-	// // /**
-	// //  * To send a text message.
-	// //  *
-	// //  */
-
-	// // // Step 1: Declare new NexmoMessage.
-	// // // $api_key = '9e300837';
-	// // // $api_secret = '7fed3068';
-	// // $nexmo_sms = new NexmoMessage('9e300837', '7fed3068');
-	// // echo "declared new nexmo message";
-	// // // Step 2: Use sendText( $to, $from, $message ) method to send a message. 
-	// // $info = $nexmo_sms->sendText( '+27740184931', 'MyApp', 'Hello!' );
-
-	// // // Step 3: Display an overview of the message
-	// // echo "sending message";
-	// // echo $nexmo_sms->displayOverview($info);
-
-	// // // Done!
-	// echo "hello";
-	$sms = new NexmoMessage('9e300837', '7fed3068');
-    if ($sms->inboundText()) {
-		$sms->reply('You said: ' . $sms->text);
-		echo "you said";
-		echo $sms->text;
-		$team1=0;
-		$team2=0;
-		$team3=0;
-		$team4=0;
-		$team5=0;
-		$team6=0;
-		$team7=0;
-		$team8=0;
-		
-	
-		switch (parseInt($sms->text,10)) {
-		    case 1:
-		        $team1 =  1;
-		        break;
-		    case 2:
-		        $team2 = 1;
-		        break;
-		    case 3:
-		        $team3 = 1;
-		        break;
-		    case 4:
-		        $team4 =  1;
-		        break;	
-		    case 5:
-		        $team5 = 1;
-		        break;
-		    case 6:
-		        $team6 = 1;
-		        break;
-		    case 7:
-		        $team7 = 1;
-		        break;
-		    case 8:
-		        $team8 = 1;
-		        break;		    
-		    }    
-		$sql="INSERT INTO votes (team1, team2, team3, team4, team5, team6, team7, team8)
-		VALUES ($team1, $team2, $team3, $team4, $team5, $team6, $team7, $team8)";
-
-		if (!mysqli_query($con,$sql)) {
-		  die('Error: ' . mysqli_error($con));
-		}
-		mysqli_close($con);
-	}
-?>
-
-
 <!DOCTYPE html>
 	<head>
-		<title>HTML5 Bar Graph Example</title>
+		<title>votes</title>
 	</head>
 
 	<body>
@@ -134,7 +45,7 @@
 				xmlhttp.send();
 					
 			
-			}, 1000);
+			}, 6000);
 			
 
 		}());</script>
