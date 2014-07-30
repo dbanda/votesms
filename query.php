1408c99833f389e4a95f4ed0407dd219af59ccdf
Dalitso Banda <dalitsohb@gmail.com>
2014-07-30 09:24:15 +0200

    Update query.php

--- query.php
+++ query.php
@@ -2,18 +2,15 @@
 	//echo "hello";
 	error_reporting(E_ALL);
 	ini_set('display_errors',1);
-		// Create connection
+	// Create connection
 	$con=mysqli_connect("127.0.0.1","root","root","votes");
 
 	// Check connection
 	if (mysqli_connect_errno()) {
 	  //echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}
-	//echo "entering for loop";
 	for ($teamnum=1; $teamnum < 9 ; $teamnum++) { 
-		//echo $teamnum;
 	 	$sql="SELECT SUM(team$teamnum) AS val FROM votes;";
-	 	//echo "$sql";
 	 	$result = mysqli_query($con,$sql)->fetch_row();
 	 	echo $result[0] . "<br>";
 	}
